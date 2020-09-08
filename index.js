f2ad2cf9dad2a18afd30b3205692bb9bdb6c105b
Zach Drummond <zachdrummond3@gmail.com>
2020-09-08 18:21:37 -0400

    Assigned id names and targeted them using JavaScript

---
+++ index.js
@@ -0,0 +1,4 @@
+var frontPage = document.getElementById("frontPage");
+var section = document.getElementById("section");
+var highScores = document.getElementById("highScores");
+var timer = document.getElementById("timer");
